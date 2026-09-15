de42a98d895f67ea780b48d8d323951469898ab3
Filipe <filipe.queiroz.abreu@gmail.com>
2026-09-15 20:27:42 -0300

    ec2_role

---
+++ ec2_role/ec2_role.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role_s3_filipe"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_role_policy" {
+  name = "ec2_role_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListAllMyBuckets",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
